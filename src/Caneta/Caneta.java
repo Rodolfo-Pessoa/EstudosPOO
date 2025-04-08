d4f554e68b0a2f09cceeff4b61dee698028eb490
Rodolfo-Pessoa <rodolfolujcaspessoa@hotmail.com>
2025-04-08 17:28:02 -0300

    POO "CANETA"

--- src/Caneta/Caneta.java
+++ src/Caneta/Caneta.java
@@ -1,4 +1,45 @@
 package Caneta;
 
 public class Caneta {
+
+  //Esses são referências à atributos:
+
+  public String modelo;
+  public String cor;
+  public float ponta;
+  public int carga;
+  public boolean tampada;
+
+  //Essa é uma referência ao método
+  //OBS: MÉTODO SEM TEM ()
+  //this. é para chamar o objeto, nesse caso quem tá chamando é c1
+
+    public void status() {
+        System.out.println("Uma caneta, " + this.cor + ";");
+        System.out.println("Ponta, " + this.ponta + ";");
+        System.out.println("Tampada? " + this.tampada + ";");
+        System.out.println("Modelo, " + this.modelo + ";");
+        System.out.println("Carga, " + this.carga + "%.");
+    }
+
+   public void tampar(){
+      this.tampada = true;
+    }
+
+   public void destampar() {
+      this.tampada = false;
+
+  }
+   public void rabiscar() {
+
+      if (this.tampada == true) {
+        System.out.println("Pode riscar!");
+      }
+      else if (this.tampada == false){
+        System.out.println("Não pode riscar, caneta tampada");
+      }
+
+    }
 }
+
+
--- src/EstudoPOO.java
+++ src/EstudoPOO.java
@@ -1,12 +1,25 @@
 import Caneta.Caneta;
+import java.lang.classfile.instruction.LocalVariable;
 import java.util.Scanner;
 import java.util.Locale;
 
-public class Caneta {
+    public class EstudoPOO {
+
+        Scanner sc = new Scanner(System.in);
+
+        public static void main(String[] args) {
+            Caneta c1 = new Caneta();
+
+            c1.modelo = "Bic Cristal";
+            c1.cor = "Azul";
+            c1.ponta = 0.5f;
+            c1.carga = 90;
+
+            c1.destampar();
+            c1.status();
+            c1.rabiscar();
+        }
 
 
-    public static void main(String[] args) {
 
-
-    }
 }
