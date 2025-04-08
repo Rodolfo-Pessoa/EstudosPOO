71a2534d8de6e900b8b3384c3acd83c389ae995c
Rodolfo-Pessoa <rodolfolujcaspessoa@hotmail.com>
2025-04-08 17:25:19 -0300

    Exercitando POO

---
+++ src/Caneta/Caneta.java
@@ -0,0 +1,4 @@
+package Caneta;
+
+public class Caneta {
+}
---
+++ src/EstudoPOO.java
@@ -0,0 +1,12 @@
+import Caneta.Caneta;
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Caneta {
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
